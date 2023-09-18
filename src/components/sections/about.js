609951e89cbaf6ea895800d8f51fd7b8916b036e
senenpalanca97 <senen@trialing.org>
2023-09-18 13:15:26 +0200

    Update skills

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -126,7 +126,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['Flutter', 'Firebase', 'Django (Python backend)', 'git  ', 'CI/CD deployment', 'C# & .Net Core 3','C', 'SAPUI5', 'SpringBoot', 'Java Hibernate', 'oData Services with Apache oLingo', 'ABAP', 'Dynamics NAV'];
+  const skills = ['Flutter', 'Firebase', 'Django', 'git', 'Agile Methodologies', 'SOLID', 'CI/CD', 'UI/UX', 'Google Cloud', 'AWS', 'Technical lead', 'Full-Stack development', 'SAP', 'ABAP', 'Dynamics NAV'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
